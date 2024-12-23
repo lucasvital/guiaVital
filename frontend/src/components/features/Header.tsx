4b51051c646b0338f1fc36c22ed4ebf8d91ad553
Lucas Vital <148392398+lucasvital@users.noreply.github.com>
2024-12-23 17:30:31 -0300

    ahahahahah

--- frontend/src/components/features/Header.tsx
+++ frontend/src/components/features/Header.tsx
@@ -72,7 +72,6 @@ export function Header() {
         <div className="flex flex-1 items-center justify-end space-x-2">
           <nav className="flex items-center space-x-2">
             {user && <NotificationBell />}
-            {user && <UserAvatar user={user} />}
             <AuthButton />
           </nav>
         </div>
